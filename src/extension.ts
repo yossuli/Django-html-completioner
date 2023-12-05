e105f9f9815d519531553cd2ba0011f2526b4c4f
yossuli <s1f102302348@iniad.org>
2023-12-05 13:05:16 +0900

    ver 0.1.0

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { execSync } from "child_process";
+import fs from "fs";
+import path from "path";
 import * as vscode from "vscode";
-
+import type { Module, stmt } from "./type";
 export function activate(context: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "extension-exercise" is now active!'
@@ -24,249 +26,139 @@ export function activate(context: vscode.ExtensionContext) {
         return;
       }
       const document = editor.document;
-      const selection = editor.selection;
-      const text = document.getText(selection);
-      console.log(text);
-      // vscode.window.showInformationMessage(text);
-    }
-  );
-
-  const disposable3 = vscode.commands.registerCommand(
-    "extension-exercise.3",
-    () => {
-      const editor = vscode.window.activeTextEditor;
-      if (editor === undefined) {
-        return;
-      }
-
-      const pathArray = editor.document.uri.path.split("/");
-
-      const viewsPyPath = `${pathArray
-        .slice(
-          0,
-          pathArray.findIndex((t) => t === "templates")
-        )
-        .join("/")}/views.py`;
-      const thisFilePath = pathArray
-        .slice(
-          pathArray.findIndex((t) => t === "templates") + 1,
-          pathArray.length
-        )
-        .join("/");
-
-      try {
-        console.log(thisFilePath);
-        const command = `python3 -c 'import ast\nimport json\ndef ast_to_json(node):\n    if isinstance(node, ast.AST):\n        field = {\n            field: ast_to_json(getattr(node, field)) for field in node._fields\n        }\n        field["type"] = type(node).__name__\n        return field\n    elif isinstance(node, list):\n        return [ast_to_json(item) for item in node]\n    else:\n        return node\nprint(json.dumps(ast_to_json(ast.parse(open("${viewsPyPath}").read()))))'`;
-
-        type AST = any;
-        // Record<string, Record<string, any>[]>;
-
-        type Variable = { id: string | null; variables: string[] | null };
-        const astJSObject: AST = JSON.parse(
-          execSync(command, {
-            encoding: "utf-8",
-          })
-        );
-        const findThisFilePath = (ast: AST): Variable => {
-          if ("body" in ast) {
-            for (const a of ast["body"]) {
-              const variable = findThisFilePath(a);
-              if (variable.id !== null) {
-                if (variable.variables !== null) {
-                  return variable;
-                }
-                for (const b of ast["body"]) {
-                  if (
-                    "targets" in b &&
-                    (b["targets"] as any[]).some(
-                      (c: any) => c["id"] === variable.id
-                    )
-                  ) {
-                    return {
-                      ...variable,
-                      variables: b["value"]["keys"].map((d: any) => d["value"]),
-                    };
-                  }
-                }
-              }
-            }
+      // const selection = editor.selection;
+      // const text = document.getText(selection);
+      // console.log(text);
+      const pathArray = document.uri.path.split("/");
+      const appPath = pathArray
+        .map((_, i) => pathArray.slice(0, pathArray.length - i).join("/"))
+        .find((path) => {
+          console.log(path);
+          try {
+            fs.statSync(`${path}/views.py`);
+            return true;
+          } catch {
+            return false;
           }
-          if (
-            ast?.type === "Return" &&
-            ast["value"]["func"]["id"] === "render"
-          ) {
-            if (ast["value"]["args"][1]["value"] === thisFilePath) {
-              return { id: ast["value"]["args"][2]["id"], variables: null };
-            }
-          }
-          return { id: null, variables: null };
-        };
-
-        console.log(findThisFilePath(astJSObject).variables);
-      } catch (error) {
-        console.error("Error extracting variable value:", error);
-      }
+        });
+      console.log(appPath);
+      if (appPath === undefined) return;
+      console.log(pathArray.slice(0, -1).join("/"), appPath);
+      const relativeRootPath = path.relative(
+        pathArray.slice(0, -1).join("/"),
+        appPath
+      );
+      console.log(relativeRootPath);
     }
   );
 
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable2);
-  context.subscriptions.push(disposable3);
 
-  const provider1 = vscode.languages.registerCompletionItemProvider("html", {
-    provideCompletionItems(
-      document: vscode.TextDocument,
-      position: vscode.Position,
-      token: vscode.CancellationToken,
-      context: vscode.CompletionContext
-    ) {
-      const editor = vscode.window.activeTextEditor;
-      if (editor === undefined) {
-        return;
-      }
+  const provider1 = vscode.languages.registerCompletionItemProvider(
+    "django-html",
+    {
+      provideCompletionItems() {
+        const editor = vscode.window.activeTextEditor;
+        if (editor === undefined) {
+          return;
+        }
 
-      const pathArray = editor.document.uri.path.split("/");
+        const pathArray = editor.document.uri.path.split("/");
+        const appPath = pathArray.slice(0, -3).join("/");
+        const thisFilePath = pathArray.slice(-2).join("/");
 
-      const viewsPyPath = `${pathArray
-        .slice(
-          0,
-          pathArray.findIndex((t) => t === "templates")
-        )
-        .join("/")}/views.py`;
-      const thisFilePath = pathArray
-        .slice(
-          pathArray.findIndex((t) => t === "templates") + 1,
-          pathArray.length
-        )
-        .join("/");
+        try {
+          const commandRows = [
+            `import ast`,
+            `import json`,
+            `def ast_to_json(node):`,
+            `    if isinstance(node, ast.AST):`,
+            `        field = {`,
+            `            field: ast_to_json(getattr(node, field)) for field in node._fields`,
+            `        }`,
+            `        field["nodeType"] = type(node).__name__`,
+            `        return field`,
+            `    elif isinstance(node, list):`,
+            `        return [ast_to_json(item) for item in node]`,
+            `    else:`,
+            `        return node`,
+            `print(json.dumps(ast_to_json(ast.parse(open("${appPath}/views.py").read()))))`,
+          ];
+          const command = `python3 -c '${commandRows.join("\n")}'`;
 
-      try {
-        console.log(thisFilePath);
-        const command = `python3 -c 'import ast\nimport json\ndef ast_to_json(node):\n    if isinstance(node, ast.AST):\n        field = {\n            field: ast_to_json(getattr(node, field)) for field in node._fields\n        }\n        field["type"] = type(node).__name__\n        return field\n    elif isinstance(node, list):\n        return [ast_to_json(item) for item in node]\n    else:\n        return node\nprint(json.dumps(ast_to_json(ast.parse(open("${viewsPyPath}").read()))))'`;
-
-        type AST = any;
-        // Record<string, Record<string, any>[]>;
-
-        type Variable = { id: string | null; variables: string[] | null };
-        const astJSObject: AST = JSON.parse(
-          execSync(command, {
-            encoding: "utf-8",
-          })
-        );
-        const findThisFilePath = (ast: AST): Variable => {
-          if ("body" in ast) {
-            for (const a of ast["body"]) {
-              const variable = findThisFilePath(a);
-              if (variable.id !== null) {
-                if (variable.variables !== null) {
-                  return variable;
-                }
-                for (const b of ast["body"]) {
-                  if (
-                    "targets" in b &&
-                    (b["targets"] as any[]).some(
-                      (c: any) => c["id"] === variable.id
-                    )
-                  ) {
-                    return {
-                      ...variable,
-                      variables: b["value"]["keys"].map((d: any) => d["value"]),
-                    };
+          type Variable = { id: string | null; variables: string[] | null };
+          const astJSObject: Module = JSON.parse(
+            execSync(command, {
+              encoding: "utf-8",
+            })
+          );
+          const findThisFilePath = (ast: stmt | Module): Variable => {
+            if ("body" in ast) {
+              for (const a of ast.body) {
+                const variable = findThisFilePath(a);
+                if (variable.id !== null) {
+                  if (variable.variables !== null) {
+                    return variable;
+                  }
+                  for (const b of ast.body) {
+                    if (
+                      "targets" in b &&
+                      "value" in b &&
+                      "keys" in b.value &&
+                      b.targets.some((c: any) => c.id === variable.id)
+                    ) {
+                      console.log(b.value);
+                      return {
+                        ...variable,
+                        variables: b.value.keys.map((d: any) => d.value),
+                      };
+                    }
                   }
                 }
               }
             }
-          }
-          if (
-            ast?.type === "Return" &&
-            ast["value"]["func"]["id"] === "render"
-          ) {
-            if (ast["value"]["args"][1]["value"] === thisFilePath) {
-              return { id: ast["value"]["args"][2]["id"], variables: null };
+            if (
+              ast?.nodeType === "Return" &&
+              ast.value &&
+              "func" in ast.value &&
+              "id" in ast.value.func &&
+              ast.value.func.id === "render"
+            ) {
+              console.log(ast.value.args[1]);
+              if (
+                "value" in ast.value.args[1] &&
+                "id" in ast.value.args[2] &&
+                ast.value.args[1].value === thisFilePath
+              ) {
+                return { id: ast.value.args[2].id, variables: null };
+              }
             }
-          }
-          return { id: null, variables: null };
-        };
-
-        const variables = findThisFilePath(astJSObject).variables;
-        console.log(variables);
-        if (variables === null) return;
-
-        return variables.map((a) => {
-          const commandCompletion = new vscode.CompletionItem(
-            a,
-            vscode.CompletionItemKind.Method
-          );
-          commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-          commandCompletion.insertText = "new ";
-          commandCompletion.command = {
-            command: "editor.action.triggerSuggest",
-            title: "Re-trigger completions...",
+            return { id: null, variables: null };
           };
-          return commandCompletion;
-        });
-      } catch (error) {
-        console.error("Error extracting variable value:", error);
-      }
-    },
-  });
-  const provider2 = vscode.languages.registerCompletionItemProvider(
-    "plaintext",
-    {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken,
-        context: vscode.CompletionContext
-      ) {
-        // a simple completion item which inserts `Hello World!`
-        const simpleCompletion = new vscode.CompletionItem("Hello World!");
 
-        // a completion item that inserts its text as snippet,
-        // the `insertText`-property is a `SnippetString` which will be
-        // honored by the editor.
-        const snippetCompletion = new vscode.CompletionItem(
-          "Good part of the day"
-        );
-        snippetCompletion.insertText = new vscode.SnippetString(
-          "Good ${1|morning,afternoon,evening|}. It is ${1}, right?"
-        );
-        snippetCompletion.documentation = new vscode.MarkdownString(
-          "Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting."
-        );
+          const variables = findThisFilePath(astJSObject).variables;
+          console.log(
+            `[info - ${Date()}] this html file get variables : [${variables}]`
+          );
+          if (variables === null) return;
 
-        // a completion item that can be accepted by a commit character,
-        // the `commitCharacters`-property is set which means that the completion will
-        // be inserted and then the character will be typed.
-        const commitCharacterCompletion = new vscode.CompletionItem("console");
-        commitCharacterCompletion.commitCharacters = ["."];
-        commitCharacterCompletion.documentation = new vscode.MarkdownString(
-          "Press `.` to get `console.`"
-        );
-
-        // a completion item that retriggers IntelliSense when being accepted,
-        // the `command`-property is set which the editor will execute after
-        // completion has been inserted. Also, the `insertText` is set so that
-        // a space is inserted after `new`
-        const commandCompletion = new vscode.CompletionItem("new");
-        commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-        commandCompletion.insertText = "new ";
-        commandCompletion.command = {
-          command: "editor.action.triggerSuggest",
-          title: "Re-trigger completions...",
-        };
-
-        // return all completion items as array
-        return [
-          simpleCompletion,
-          snippetCompletion,
-          commitCharacterCompletion,
-          commandCompletion,
-        ];
+          return variables.map((a) => {
+            const newCompletionItem = new vscode.CompletionItem(
+              a,
+              vscode.CompletionItemKind.Variable
+            );
+            newCompletionItem.detail = "../../views.py";
+            return newCompletionItem;
+          });
+        } catch (error) {
+          console.error("Error extracting variable value:", error);
+        }
       },
     }
   );
-  context.subscriptions.push(provider1, provider2);
+
+  context.subscriptions.push(provider1);
 }
 
 export function deactivate() {}
