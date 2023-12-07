5ac18f3df504010c7f3ac8e5bb0fafab800060c1
yossuli <s1f102302348@iniad.org>
2023-12-07 17:13:55 +0900

    add color code

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -2,16 +2,36 @@ import * as path from "path";
 
 import { runTests } from "@vscode/test-electron";
 
+const black = "\u001b[30m";
+const red = "\u001b[31m";
+const green = "\u001b[32m";
+const yellow = "\u001b[33m";
+const blue = "\u001b[34m";
+const magenta = "\u001b[35m";
+const cyan = "\u001b[36m";
+const white = "\u001b[37m";
+const reset = "\u001b[0m";
+
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+    console.log(
+      `${cyan}extensionDevelopmentPath is ${JSON.stringify(
+        extensionDevelopmentPath
+      )}${reset}`
+    );
 
     // The path to test runner
     // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, "./suite/index");
 
+    console.log(
+      `${cyan}extensionTestsPath is ${JSON.stringify(
+        extensionTestsPath
+      )}${reset}`
+    );
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
