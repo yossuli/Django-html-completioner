e0ac486e97d6ee5b656e65daef481b86b353bd50
yossuli <s1f102302348@iniad.org>
2024-01-21 23:30:10 +0900

    2回目がsetupされない

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -33,73 +33,86 @@ suite("Extension Test Suite", async () => {
     item: string;
     path?: string;
   }[] = [];
-  setup(async () => {
-    consoleColorLog(`set up`, "cyan");
-    if (vscode.workspace.workspaceFolders === undefined) {
-      consoleColorLog(`workspace folders is undefined`, "cyan");
-      assert.fail();
-    }
-    // for (const testFileLocationAndExpectedItem of testFileLocationAndExpectedItems) {
-    const testFileLocationAndExpectedItem = testFileLocationAndExpectedItems[0];
-      consoleColorLog("set up 1", testFileLocationAndExpectedItem.color);
+  const testProcess = (testCount: number): Promise<void> =>
+    new Promise((resolve) => {
+      console.log(`<<<${testCount}>>>`);
+      setup(async () => {
+        consoleColorLog(`set up`, "cyan");
+        if (vscode.workspace.workspaceFolders === undefined) {
+          consoleColorLog(`workspace folders is undefined`, "cyan");
+          assert.fail();
+        }
+        // for (const testFileLocationAndExpectedItem of testFileLocationAndExpectedItems) {
+        const testFileLocationAndExpectedItem =
+          testFileLocationAndExpectedItems[testCount];
+        consoleColorLog("set up 1", testFileLocationAndExpectedItem.color);
 
-      await sleep(1000);
-      const filePath =
-        vscode.workspace.workspaceFolders[0].uri.fsPath +
-        testFileLocationAndExpectedItem.location;
+        await sleep(1000);
+        const filePath =
+          vscode.workspace.workspaceFolders[0].uri.fsPath +
+          testFileLocationAndExpectedItem.location;
 
-      await sleep(1000);
-      const fileUri = vscode.Uri.file(filePath);
-      consoleColorLog("set up 2", testFileLocationAndExpectedItem.color);
+        await sleep(1000);
+        const fileUri = vscode.Uri.file(filePath);
+        consoleColorLog("set up 2", testFileLocationAndExpectedItem.color);
 
-      await sleep(1000);
-      const document = await vscode.workspace.openTextDocument(fileUri);
-      consoleColorLog("set up 3", testFileLocationAndExpectedItem.color);
+        await sleep(1000);
+        const document = await vscode.workspace.openTextDocument(fileUri);
+        consoleColorLog("set up 3", testFileLocationAndExpectedItem.color);
 
-      await sleep(1000);
-      editorAndExpectedItems.push({
-        editor: await vscode.window.showTextDocument(document),
-        item: testFileLocationAndExpectedItem.item,
-        path: fileUri.path,
+        await sleep(1000);
+        editorAndExpectedItems.push({
+          editor: await vscode.window.showTextDocument(document),
+          item: testFileLocationAndExpectedItem.item,
+          path: fileUri.path,
+        });
+        consoleColorLog("set up 4", testFileLocationAndExpectedItem.color);
+        consoleColorLog(`editorAndExpectedItem is ${fileUri.path}`, "magenta");
+        // }
       });
-      consoleColorLog("set up 4", testFileLocationAndExpectedItem.color);
-      consoleColorLog(`editorAndExpectedItem is ${fileUri.path}`, "magenta");
-      // }
-    });
-    test("Completion Items are Provided", async function () {
-      // for (const editorAndExpectedItem of editorAndExpectedItems) {
-      const editorAndExpectedItem = editorAndExpectedItems[0];
-      
-      consoleColorLog(
-        `uri is ${editorAndExpectedItem.editor.document.uri}`,
-        testFileLocationAndExpectedItems[0].color
-      );
-      const completionList =
-        await vscode.commands.executeCommand<vscode.CompletionList>(
-          "vscode.executeCompletionItemProvider",
-          editorAndExpectedItem.editor.document.uri,
-          new vscode.Position(0, 0)
+      console.log(`<<<<${testCount}>>>>`);
+      test(`Completion Items are Provided - ${testCount}`, async function () {
+        // for (const editorAndExpectedItem of editorAndExpectedItems) {
+        const editorAndExpectedItem = editorAndExpectedItems[testCount];
+
+        consoleColorLog(
+          `uri is ${editorAndExpectedItem.editor.document.uri}`,
+          testFileLocationAndExpectedItems[testCount].color
+        );
+        const completionList =
+          await vscode.commands.executeCommand<vscode.CompletionList>(
+            "vscode.executeCompletionItemProvider",
+            editorAndExpectedItem.editor.document.uri,
+            new vscode.Position(0, 0)
+          );
+
+        consoleColorLog(
+          `completionList is ${JSON.stringify(completionList)}`,
+          testFileLocationAndExpectedItems[testCount].color
         );
 
-      consoleColorLog(
-        `completionList is ${JSON.stringify(completionList)}`,
-        testFileLocationAndExpectedItems[0].color
-      );
-
-      assert.ok(completionList.items.length > 0);
-      const completionItem = completionList.items[0];
-      assert.strictEqual(completionItem.label, editorAndExpectedItem.item);
-      assert.strictEqual(
-        completionItem.kind,
-        vscode.CompletionItemKind.Variable
-      );
-      assert.strictEqual(completionItem.detail, "../../views.py");
-      vscode.window.showInformationMessage("End all tests.");
-      await vscode.commands.executeCommand(
-        "workbench.action.closeActiveEditor"
-      );
-    // }
-  });
+        assert.ok(completionList.items.length > 0);
+        const completionItem = completionList.items[0];
+        assert.strictEqual(completionItem.label, editorAndExpectedItem.item);
+        assert.strictEqual(
+          completionItem.kind,
+          vscode.CompletionItemKind.Variable
+        );
+        assert.strictEqual(completionItem.detail, "../../views.py");
+        vscode.window.showInformationMessage("End all tests.");
+        await sleep(500);
+        await vscode.commands.executeCommand(
+          "workbench.action.closeActiveEditor"
+        );
+        // }
+        console.log(`<<<<<${testCount}>>>>>`);
+        resolve();
+      });
+    });
+  await testProcess(0);
+  consoleColorLog("test1", "green");
+  await testProcess(1);
+  consoleColorLog("test2", "yellow");
   teardown(async function () {
     await vscode.commands.executeCommand("vscode.setEditorLayout", {
       groups: [{}, {}],
@@ -107,5 +120,4 @@ suite("Extension Test Suite", async () => {
     });
     await vscode.commands.executeCommand("workbench.action.closeAllEditors");
   });
-  
 });
