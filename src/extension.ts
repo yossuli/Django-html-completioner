5f40a3b7f99f708b493f41defc2d7b6317f76a34
yossuli <s1f102302348@iniad.org>
2024-01-24 21:31:51 +0900

    separate 機能で分割

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode";
-import { findTemplateNameFromModule } from "./utils/@djangoHtmlCompletion/findTemplateNameFromModule";
-import { viewsPyToASTObject } from "./utils/@djangoHtmlCompletion/pythonFileToASTObject";
+import { djangoHTMLCompletionItemProvider } from "./extensions/djangoHTMLCompletionItemProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
@@ -24,47 +23,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
   context.subscriptions.push(disposable2);
 
-  const provider1 = vscode.languages.registerCompletionItemProvider(
-    ["django-html"],
-    {
-      provideCompletionItems() {
-        const editor = vscode.window.activeTextEditor;
-        if (editor === undefined) {
-          return;
-        }
-
-        const pathArray = editor.document.uri.path.split("/");
-        const appPath = pathArray.slice(0, -3).join("/");
-        const templateName = pathArray.slice(-2).join("/");
-
-        try {
-          const astJSObject = viewsPyToASTObject(appPath);
-
-          const variables = findTemplateNameFromModule(
-            astJSObject,
-            templateName
-          ).variables;
-          if (variables === null) return;
-
-          console.log(
-            `[info - ${new Date().toLocaleTimeString()}] ${templateName} get variables : [${variables}]`
-          );
-          return variables.map((a) => {
-            const newCompletionItem = new vscode.CompletionItem(
-              a,
-              vscode.CompletionItemKind.Variable
-            );
-            newCompletionItem.detail = "../../views.py";
-            return newCompletionItem;
-          });
-        } catch (error) {
-          console.error("Error extracting variable value:", error);
-        }
-      },
-    }
-  );
-
-  context.subscriptions.push(provider1);
+  context.subscriptions.push(djangoHTMLCompletionItemProvider);
 }
 
 export function deactivate() {}
---
+++ src/extensions/djangoHTMLCompletionItemProvider.ts
@@ -0,0 +1,41 @@
+import * as vscode from "vscode";
+import { findTemplateNameFromModule } from "../utils/@djangoHtmlCompletion/fromModule";
+import { viewsPyToASTObject } from "../utils/@djangoHtmlCompletion/pythonFileToASTObject";
+
+export const djangoHTMLCompletionItemProvider =
+  vscode.languages.registerCompletionItemProvider(["django-html"], {
+    provideCompletionItems() {
+      const editor = vscode.window.activeTextEditor;
+      if (editor === undefined) {
+        return;
+      }
+
+      const pathArray = editor.document.uri.path.split("/");
+      const appPath = pathArray.slice(0, -3).join("/");
+      const templateName = pathArray.slice(-2).join("/");
+
+      try {
+        const astJSObject = viewsPyToASTObject(appPath);
+
+        const variables = findTemplateNameFromModule(
+          astJSObject,
+          templateName
+        ).variables;
+        if (variables === null) return;
+
+        console.log(
+          `[info - ${new Date().toLocaleTimeString()}] ${templateName} get variables : [${variables}]`
+        );
+        return variables.map((a) => {
+          const newCompletionItem = new vscode.CompletionItem(
+            a,
+            vscode.CompletionItemKind.Variable
+          );
+          newCompletionItem.detail = "../../views.py";
+          return newCompletionItem;
+        });
+      } catch (error) {
+        console.error("Error extracting variable value:", error);
+      }
+    },
+  });
