880a772216f199a6c82128bd5c49e25062956a85
yossuli <s1f102302348@iniad.org>
2024-01-25 00:31:16 +0900

    add views.pyで呼ばれているかを確認

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+import * as path from "path";
 import * as vscode from "vscode";
 import { djangoHTMLCompletionItemProvider } from "./extensions/djangoHTMLCompletionItemProvider";
 
@@ -15,16 +17,50 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
+  // extension.ts
+
+  // export function activate(context: vscode.ExtensionContext) {
   const onDidOpenTextDocument = vscode.workspace.onDidOpenTextDocument((e) => {
     if (e.languageId === "django-html") {
-      vscode.window.showInformationMessage(
-        "django-htmlファイルが開かれました!"
-      );
+      const pathArray = e.uri.path.split("/");
+      const appPath = pathArray.slice(0, -3).join("/");
+      const viewsPath = path.join(appPath, "views.py");
+
+      // views.pyを読み込みます。
+      fs.readFile(viewsPath, "utf8", (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const templateName = path.basename(e.fileName, ".html");
+
+        const openFileCommandUri = vscode.Uri.parse(
+          `command:vscode.open?${encodeURIComponent(
+            JSON.stringify([vscode.Uri.file(viewsPath)])
+          )}`
+        );
+
+        const recheckCommandUri = vscode.Uri.parse(
+          `command:extension.recheckRender?${encodeURIComponent(
+            JSON.stringify([viewsPath])
+          )}`
+        );
+
+        vscode.window.showWarningMessage(
+          `'${templateName}.html'はviews.pyに存在${
+            !data.match(`${templateName}.html`)
+              ? "しない可能性があります"
+              : "します"
+          }`
+        );
+      });
     }
   });
 
-  context.subscriptions.push(disposable);
   context.subscriptions.push(onDidOpenTextDocument);
+
+  context.subscriptions.push(disposable);
 
   context.subscriptions.push(djangoHTMLCompletionItemProvider);
 }
