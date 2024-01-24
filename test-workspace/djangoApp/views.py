d078a0778e00012037808b4d6d459e3cd63b1bab
yossuli <s1f102302348@iniad.org>
2024-01-24 21:32:04 +0900

    add test case

--- test-workspace/djangoApp/views.py
+++ test-workspace/djangoApp/views.py
@@ -1,6 +1,8 @@
 # pyright: reportUndefinedVariable=false
 def index(request):
     if a:
+        if b:
+            return render(request,'djangoApp/index3.html')
         context={'test2_1':'value','test2_2':'value'}
         return render(request,'djangoApp/index2.html',context)
     context={'test1':'value'}
