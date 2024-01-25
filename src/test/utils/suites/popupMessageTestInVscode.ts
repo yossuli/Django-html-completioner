4ad41edf0c5a37d4c26c2c264de8cff0f90023a7
yossuli <s1f102302348@iniad.org>
2024-01-26 01:06:54 +0900

    一旦

--- src/test/utils/suites/popupMessageTestInVscode.ts
+++ src/test/utils/suites/popupMessageTestInVscode.ts
@@ -15,17 +15,25 @@ export const popupMessageTestInVscode = (a: PopupMassageItemsTestCases) => {
   let editor: vscode.TextEditor;
   let showWarningMessageSpy: sinon.SinonSpy;
   let showInformationMessageSpy: sinon.SinonSpy;
+  // let showWarningMessageStub: sinon.SinonStub;
+  let showTextDocumentStub: sinon.SinonStub;
+  let openTextDocumentStub: sinon.SinonStub;
   const consoleLogColor: ConsoleLogColor = "magenta";
   // ANCHOR setup
   setup(async () => {
-    editor = await setupEditor(a.location);
-    await sleep(100);
-
     showWarningMessageSpy = sinon.spy(vscode.window, "showWarningMessage");
     showInformationMessageSpy = sinon.spy(
       vscode.window,
       "showInformationMessage"
     );
+    // showWarningMessageStub = sinon.stub(vscode.window, "showWarningMessage");
+    await sleep(100);
+
+    editor = await setupEditor(a.location);
+    await sleep(100);
+
+    openTextDocumentStub = sinon.stub(vscode.workspace, "openTextDocument");
+    showTextDocumentStub = sinon.stub(vscode.window, "showTextDocument");
     await sleep(100);
 
     consoleColorLog("End setup.", consoleLogColor);
@@ -35,33 +43,47 @@ export const popupMessageTestInVscode = (a: PopupMassageItemsTestCases) => {
   test(`Popup Message in ${fileName}`, async function () {
     consoleColorLog("execute command", consoleLogColor);
 
-    vscode.commands.executeCommand(
-      "extension-exercise.checkRendering",
-      editor.document.uri
-    );
-    // vscode.window.showWarningMessage("dummy message");
-
     await sleep(1000);
-    assert.strictEqual(showWarningMessageSpy.callCount > 0, !a.isCalled);
-    assert.strictEqual(showInformationMessageSpy.callCount > 0, a.isCalled);
+    assert.strictEqual(
+      showWarningMessageSpy.callCount,
+      a.isCalled ? 0 : 1,
+      "showWarningMessageSpy"
+    );
+    // assert.strictEqual(showWarningMessageStub.callCount , a.isCalled ? 0 : 1,'showWarningMessageStub');
+    assert.strictEqual(
+      showInformationMessageSpy.callCount,
+      !a.isCalled ? 0 : 1,
+      "showInformationMessageSpy"
+    );
+
     if (a.isCalled) {
       assert.strictEqual(
         showInformationMessageSpy.getCall(0).args[0],
         `'${fileName}'はviews.pyで呼び出されています`
       );
-    } else {
+      assert.strictEqual(showInformationMessageSpy.getCall(0).args.length, 1);
+    } //
+    else {
+      // assert.strictEqual(
+      //   showWarningMessageStub.getCall(0).args[0],
+      //   `'${fileName}'はviews.pyで呼び出されていない可能性があります`
+      // );
       assert.strictEqual(
         showWarningMessageSpy.getCall(0).args[0],
         `'${fileName}'はviews.pyで呼び出されていない可能性があります`
       );
+      // assert.strictEqual(showWarningMessageStub.getCall(0).args.length, 3);
+      assert.strictEqual(showWarningMessageSpy.getCall(0).args.length, 3);
     }
-    await sleep(1000);
+    await sleep(100);
   });
   teardown(cleanupVscode);
   teardown(() => {
-    // スタブをリセット
+    // showWarningMessageStub.restore();
     showWarningMessageSpy.restore();
     showInformationMessageSpy.restore();
+    showTextDocumentStub.restore();
+    openTextDocumentStub.restore();
     consoleColorLog("stubs reset", consoleLogColor);
   });
 };
