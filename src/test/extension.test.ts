d85b84482ed997b21d70add4b57fb8cb402b5c7a
yossuli <s1f102302348@iniad.org>
2024-01-25 10:57:20 +0900

    move

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import type { TestFileLocationAndExpectedItems } from "./../types/types";
-import { completionItemsTestInVscode } from "./utils/completionItemsTestInVscode";
+import { completionItemsTestInVscode } from "./utils/suites/completionItemsTestInVscode";
 
 const testFileLocationAndExpectedItems: TestFileLocationAndExpectedItems[] = [
   {
