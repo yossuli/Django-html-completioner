0e62369ce157ce219fc31dc8602f822687e05d09
yossuli <s1f102302348@iniad.org>
2024-01-27 22:15:01 +0900

    fix windowsで動くように？3

--- src/utils/AST/pythonFileToASTObject.ts
+++ src/utils/AST/pythonFileToASTObject.ts
@@ -16,11 +16,9 @@ export const viewsPyToASTObject = (appPath: string): Module => {
     `        return [ast_to_json(item) for item in node]`,
     `    else:`,
     `        return node`,
-    `print(json.dumps(ast_to_json(ast.parse(open("${appPath}/views.py").read()))))`,
+    `print(json.dumps(ast_to_json(ast.parse(open('${appPath}/views.py').read()))))`,
   ];
-  const command = `python3 -c '''
-${commandRows.join("\n")}
-'''`;
+  const command = `python3 -c "${commandRows.join("\n")}"`;
 
   return JSON.parse(
     execSync(command, {
